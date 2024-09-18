7ae6fa039d2bb1521dda4c7ec6b525f1330d124f
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-18 08:21:22 +0530

    add search functionality

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -34,6 +34,10 @@ const Collection = () => {
   const applyFilter = () =>{
     let productsCopy = products.slice();
 
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
     if(category.length > 0){
       productsCopy = productsCopy.filter(item => category.includes(item.category))
     }
@@ -64,7 +68,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
-  },[category,subCategory])
+  },[category,subCategory,search,showSearch])
 
   useEffect(()=>{
     sortProduct();
