7f1cdae9654019ef1e125c68ea9d183eee54ba77
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-25 09:06:46 +0530

    add login and register functionality in frontend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,49 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Login = () => {
   const [currentState, setCurrentState] = useState("Sign Up")
   const { navigate,backendUrl,token,setToken} = useContext(ShopContext)
 
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const onSubmitHandler = async (event) =>{
     event.preventDefault();
+    try {
+      if(currentState === "Sign Up"){
+        const response = await axios.post(backendUrl+ "/api/user/register",{name,email,password})
+        if(response.data.success){
+          setToken(response.data.token)
+          localStorage.setItem('token',response.data.token)
+        }else{
+          toast.error(response.data.message)
+        }       
+
+      } else{
+        const response = await axios.post(backendUrl + "/api/user/login",{email,password})
+        if(response.data.success){
+          setToken(response.data.token)
+          localStorage.setItem("token",response.data.token)
+        }else{
+          toast.error(response.data.message)
+        }
+
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
   }
+
+  useEffect(()=>{
+    if(token){
+      navigate("/")
+    }
+  },[token])
   return (
     <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
       <div className='inline-flex items-center gap-2 mb-2 mt-10'>
@@ -15,9 +51,9 @@ const Login = () => {
         <hr className='border-none h-[1.5px] w-8 bg-green-800'/>
       </div>
 
-      {currentState === "Login" ? "" :<input type="text" className='w-full px-3 py-2 border border-green-800' placeholder='Enter Name' required />}
-      <input type="email" className='w-full px-3 py-2 border border-green-800' placeholder='Enter Email' required />
-      <input type="password" className='w-full px-3 py-2 border border-green-800' placeholder='Enter Password' required />
+      {currentState === "Login" ? "" :<input onChange={(e)=>setName(e.target.value)} value={name} type="text" className='w-full px-3 py-2 border border-green-800' placeholder='Enter Name' required />}
+      <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" className='w-full px-3 py-2 border border-green-800' placeholder='Enter Email' required />
+      <input onChange={(e)=>setPassword(e.target.value)} value={password} type="password" className='w-full px-3 py-2 border border-green-800' placeholder='Enter Password' required />
       <div className='w-full flex justify-between text-sm mt-[-8px]'>
         <p className='cursor-pointer'>Forgot your password?</p>
         {currentState === "Login" ?
