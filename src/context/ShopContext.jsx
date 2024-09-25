f6b0368342bab290f8a510262f55c6bcd1c1924a
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-25 08:44:19 +0530

    connect backend to frontend to showing tha data

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/assets";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from "axios"
 
 
 export const ShopContext = createContext();
@@ -10,9 +10,12 @@ const ShopContextProvider = (props) =>{
 
     const currency = "$"
     const delivery_fee = 10
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [search,setSearch] = useState('')
     const [showSearch,setShowSearch] = useState(false)
     const [cartItems,setCartItems] = useState({})
+    const [products,setProducts] = useState([])
+    const [token,setToken]  =useState('')
     const navigate = useNavigate()
 
     const addToCart = async (itemId,size) =>{
@@ -78,10 +81,33 @@ const ShopContextProvider = (props) =>{
         return totalAmount;
     }
 
+    //get product data
+    const getProductsData = async () =>{
+        try {
+           const response = await axios.get(backendUrl+ "/api/product/list") 
+           if(response.data.success){
+            setProducts(response.data.products)
+           }else{
+            toast.error(response.data.message)
+           }
+           
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(()=>{
+        getProductsData();
+    },[])
+
 
 
     const value = {
-        products, currency, delivery_fee,search,setSearch,showSearch,setShowSearch,cartItems,addToCart,getcartCount,updateQuantity,getCartAmount,navigate
+        products, currency, delivery_fee,search,
+        setSearch,showSearch,setShowSearch,cartItems,
+        addToCart,getcartCount,updateQuantity,getCartAmount,
+        navigate,backendUrl,token,setToken
     }
 
     return (
--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -68,7 +68,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
-  },[category,subCategory,search,showSearch])
+  },[category,subCategory,search,showSearch,products])
 
   useEffect(()=>{
     sortProduct();
