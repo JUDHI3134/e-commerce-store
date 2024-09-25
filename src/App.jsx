e6dac7cc7b4eb5032b85cda86ae77964ba3a9621
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-25 15:56:24 +0530

    create verify payment page i frontend with navigate

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Footer from './components/Footer'
 import SearchBar from './components/searchBar'
 import { ToastContainer, toast } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
+import Verify from './pages/Verify'
 
 const App = () => {
   return (
@@ -31,6 +32,7 @@ const App = () => {
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/orders' element={<Orders/>}/>
         <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/verify' element={<Verify/>}/>
       </Routes>
       <Footer/>
     </div>
